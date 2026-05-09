beb425d149da49626a1bc690cd3073f6ad463fd7
Alyssa Fendler <jacobfendler@gmail.com>
2026-05-09 01:48:36 -0500

    updated card set stuff

--- words.py
+++ words.py
@@ -1,39 +1,65 @@
-print("hello bitch")
 import string
+print("hello bitch")
 
-fool_words={'adventure', 'summer', 'opportunity', 'change', 'progress', 'transition', 'hesitation'}
-magic_words={'experiment', 'trans', 'initiative', 'action', 'proactive'}
-hp_words={'intuition','stuck','midterm','exam','friendship'}
-empress_words={'project', 'idea','block','mpreg','health','inspiration'}
-emperor_words={'lockin','focus','procrastinate','study','monster','celsius'}
-hiero_words={'advice','guidance','mothers day','parent','mother','father'}
-lover_words={'path','situationship','hinge','grindr','tinder','divorce','breakup','scandal','confidence'}
-chariot_words={'goals','advisor','mentor','momentum','perseverance','promotion','responsibility','internship'}
-strength_words={'forgiveness','forgive','angry','anger','frustrated','grudge','revenge'}
-hermit_words={'professor','library','confused','lonely','quiet','study','lockin'}
-wheel_words={'repeat','again','graduate','failure','test','withdraw','adddrop', 'meaning of life'}
-justice_words={'protest','competition','war','revenge','compromise','opinion','argument'}
-hanged_words={'reflection','waiting','realize','discovery','clarity','focus'}
-death_words={'graduation','graduate','summer','quarter','end','moving','meaning of life'}
-temp_words={'grades','curve','mental health','therapy','depress','canvas','assignment'}
-devil_words={'situationship','judge','scav','procrastinate','lazy','laziness'}
-tower_words={'midterm','exam','grades','partner','boyfriend','girlfriend','government'}
-star_words={'therapy','ex','again','another try','fresh start'}
-moon_words={'uncertain','confused','experiment','tryout', 'try','interview'}
-sun_words={'stress','difficult','uchicago','scav','weather','mothers day','meaning of life'}
-judge_words={'graduate','judgment','points','item','showcase','ida noyes','david rubenstein','david m rubenstein','religion','church','god'}
-world_words={'victory','win','graduation','judgeship','celebration','time'}
-all_words_list=[fool_words, magic_words,hp_words, empress_words,emperor_words,hiero_words,lover_words,chariot_words,strength_words,hermit_words,wheel_words, justice_words, hanged_words, death_words, temp_words,devil_words, tower_words, star_words, moon_words, sun_words, judge_words, world_words]
+fool_words = {'adventure', 'summer', 'opportunity',
+              'change', 'progress', 'transition', 'hesitation'}
+magic_words = {'experiment', 'trans', 'initiative', 'action', 'proactive'}
+hp_words = {'intuition', 'stuck', 'midterm', 'exam', 'friendship'}
+empress_words = {'project', 'idea', 'block', 'mpreg', 'health', 'inspiration'}
+emperor_words = {'lockin', 'focus',
+                 'procrastinate', 'study', 'monster', 'celsius'}
+hiero_words = {'advice', 'guidance',
+               'mothers day', 'parent', 'mother', 'father'}
+lover_words = {'path', 'situationship', 'hinge', 'grindr',
+               'tinder', 'divorce', 'breakup', 'scandal', 'confidence'}
+chariot_words = {'goals', 'advisor', 'mentor', 'momentum',
+                 'perseverance', 'promotion', 'responsibility', 'internship'}
+strength_words = {'forgiveness', 'forgive', 'angry',
+                  'anger', 'frustrated', 'grudge', 'revenge'}
+hermit_words = {'professor', 'library', 'confused',
+                'lonely', 'quiet', 'study', 'lockin'}
+wheel_words = {'repeat', 'again', 'graduate', 'failure',
+               'test', 'withdraw', 'adddrop', 'meaning of life'}
+justice_words = {'protest', 'competition', 'war',
+                 'revenge', 'compromise', 'opinion', 'argument'}
+hanged_words = {'reflection', 'waiting',
+                'realize', 'discovery', 'clarity', 'focus'}
+death_words = {'graduation', 'graduate', 'summer',
+               'quarter', 'end', 'moving', 'meaning of life'}
+temp_words = {'grades', 'curve', 'mental health',
+              'therapy', 'depress', 'canvas', 'assignment'}
+devil_words = {'situationship', 'judge',
+               'scav', 'procrastinate', 'lazy', 'laziness'}
+tower_words = {'midterm', 'exam', 'grades', 'partner',
+               'boyfriend', 'girlfriend', 'government'}
+star_words = {'therapy', 'ex', 'again', 'another try', 'fresh start'}
+moon_words = {'uncertain', 'confused',
+              'experiment', 'tryout', 'try', 'interview'}
+sun_words = {'stress', 'difficult', 'uchicago', 'scav',
+             'weather', 'mothers day', 'meaning of life'}
+judge_words = {'graduate', 'judgment', 'points', 'item', 'showcase', 'ida noyes',
+               'david rubenstein', 'david m rubenstein', 'religion', 'church', 'god'}
+world_words = {'victory', 'win', 'graduation',
+               'judgeship', 'celebration', 'time'}
+all_words_list = [fool_words, magic_words, hp_words, empress_words, emperor_words, hiero_words, lover_words, chariot_words, strength_words, hermit_words,
+                  wheel_words, justice_words, hanged_words, death_words, temp_words, devil_words, tower_words, star_words, moon_words, sun_words, judge_words, world_words]
+tarot_cards = ["fool", "magic", "highpriestess", "empress", "emperor", "hierophant", "lovers", "chariot", "strength", "hermit", "wheel", "justice", "hangedman", "death",
+               "temperance", "devil", "tower", "star", "moon", "sun", "judgment", "world"]
+
 
 question_asked = input("What question do you seek answered?")
 stripped_q = question_asked.lower().split(' ')
 final_q = [w.strip(string.punctuation) for w in stripped_q]
+
+
 def main():
     return None
 
+
 def find_cards(question):
-    card_dict={x:0 for x in all_words_list}
+    card_set_dict = {x[0]: x[1] for x in zip(tarot_cards, all_words_list)}
+    card_score_dict = {x: 0 for x in tarot_cards}
     for word in question:
-        for i in all_words_list:
-            if word in i:
-                card_dict[i]+=1
+        for cardname, wordset in card_set_dict.items():
+            if word in wordset:
+                card_score_dict[word] += 1
