e022c3c3a2f4804aca36cbbea89658492232967e
Rena Hollister <renahollister@optonline.net>
2026-05-09 19:49:49 -0500

    commit #9

--- words.py
+++ words.py
@@ -2,53 +2,56 @@ import string
 print("hello bitch")
 
 fool_words = {'adventure', 'summer', 'opportunity',
-              'change', 'progress', 'transition', 'hesitation'}
-magic_words = {'experiment', 'trans', 'initiative', 'action', 'proactive'}
-hp_words = {'intuition', 'stuck', 'midterm', 'exam', 'friendship'}
-empress_words = {'project', 'idea', 'block', 'mpreg', 'health', 'inspiration'}
+              'change', 'progress', 'transition', 'hesitation', 'new',
+              'study abroad', 'profess'}
+magic_words = {'experiment', 'trans', 'initiative', 'action', 'proactive','willpower','block','job','career'}
+hp_words = {'intuition', 'stuck', 'midterms', 'exams', 'friendship', 'relationship', 'friend'}
+empress_words = {'project', 'idea', 'block', 'mpreg', 'health', 'inspiration','nature', 'intuition','job','career'}
 emperor_words = {'lockin', 'focus',
-                 'procrastinate', 'study', 'monster', 'celsius'}
+                 'procrastinate', 'study', 'monster', 'celsius', 'purpose', 'boundaries','boundary', 'career', 'cuck'}
 hiero_words = {'advice', 'guidance',
-               'mothers day', 'parent', 'mother', 'father'}
+               'mothers day', 'parent', 'mother', 'father','tradition'}
 lover_words = {'path', 'situationship', 'hinge', 'grindr',
-               'tinder', 'divorce', 'breakup', 'scandal', 'confidence'}
+               'tinder', 'divorce', 'breakup', 'scandal', 'confidence',
+               'date', 'dating', 'ask out', 'rizz', 'red flag','relationship','wedding','love'}
 chariot_words = {'goals', 'advisor', 'mentor', 'momentum',
-                 'perseverance', 'promotion', 'responsibility', 'internship'}
+                 'perseverance', 'promotion', 'responsibility', 'internship',
+                 'grindset', 'job', 'direction', 'success','purpose','profess', 'interview'}
 strength_words = {'forgiveness', 'forgive', 'angry',
-                  'anger', 'frustrated', 'grudge', 'revenge'}
+                  'anger', 'frustrated', 'grudge', 'revenge','focus', 'success', 'ida', 'noyes','career', 'virtue'}
 hermit_words = {'professor', 'library', 'confused',
-                'lonely', 'quiet', 'study', 'lockin'}
+                'lonely', 'quiet', 'study', 'lockin', 'the reg', 'think','intuition','block','nerd'}
 wheel_words = {'repeat', 'again', 'graduate', 'failure',
-               'test', 'withdraw', 'adddrop', 'meaning of life'}
+               'test', 'withdraw', 'adddrop', 'meaning of life','change', 'where','convocation'}
 justice_words = {'protest', 'competition', 'war',
-                 'revenge', 'compromise', 'opinion', 'argument'}
+                 'revenge', 'compromise', 'opinion', 'argument', 'republican', 'effect'}
 hanged_words = {'reflection', 'waiting',
-                'realize', 'discovery', 'clarity', 'focus'}
+                'realize', 'discovery', 'clarity', 'focus','release', 'seek','purpose'}
 death_words = {'graduation', 'graduate', 'summer',
-               'quarter', 'end', 'moving', 'meaning of life'}
+               'quarter', 'end', 'moving', 'meaning of life','change','beginning', 'obstacle','future','terror'}
 temp_words = {'grades', 'curve', 'mental health',
-              'therapy', 'depressed','depression', 'canvas', 'assignment'}
+              'therapy', 'depressed','depression', 'canvas', 'assignments', 
+              'finals', 'midterms','patience', 'outcome','terror'}
 devil_words = {'situationship', 'judge',
-               'scav', 'procrastinate', 'lazy', 'laziness'}
-tower_words = {'midterm', 'exam', 'grades', 'partner',
-               'boyfriend', 'girlfriend', 'government'}
-star_words = {'therapy', 'ex', 'again', 'another try', 'fresh start'}
+               'scav', 'procrastinate', 'lazy', 'laziness', 
+               'business','economics', 'finance', 'quant', 'relationship', 'bizcon'}
+tower_words = {'midterm', 'exam', 'exams', 'grades','grade', 'partner',
+               'boyfriend', 'girlfriend', 'government','disaster', 'relationship','connection','future', 'david', 'rubenstein','obama','love',
+               'terror'}
+star_words = {'therapy', 'ex', 'again', 'another', 'fresh start','faith','new','try', 'red', 'flag','year'}
 moon_words = {'uncertain', 'confused',
-              'experiment', 'tryout', 'try', 'interview'}
+              'experiment', 'tryout', 'try', 'interview',
+              'what the fuck', 'understand','confused','illusion','unsure','unclear','intuition', 'obstacles','block', 'where'}
 sun_words = {'stress', 'difficult', 'uchicago', 'scav',
-             'weather', 'mothers day', 'meaning of life'}
-judge_words = {'graduate', 'judgment', 'points', 'item', 'showcase', 'ida noyes',
-               'david rubenstein', 'david m rubenstein', 'religion', 'church', 'god'}
+             'weather', 'mothers day', 'meaning of life','positiv','success','celebrate', 'celebration','friend', 'future','virtue'}
+judge_words = {'graduate', 'judgment', 'points', 'item', 'showcase', 'ida', 'noyes',
+               'david', 'rubenstein', 'religion', 'church', 'god','reflect'}
 world_words = {'victory', 'win', 'graduation',
-               'judgeship', 'celebration', 'time'}
+               'judgeship', 'celebration', 'time','complete','completion', 'success', 'years', 'convocation','career'}
 all_words_list = [fool_words, magic_words, hp_words, empress_words, emperor_words, hiero_words, lover_words, chariot_words, strength_words, hermit_words,
                   wheel_words, justice_words, hanged_words, death_words, temp_words, devil_words, tower_words, star_words, moon_words, sun_words, judge_words, world_words]
-tarot_cards = ["fool", "magic", "highpriestess", "empress", "emperor", "hierophant", "lovers", "chariot", "strength", "hermit", "wheel", "justice", "hangedman", "death",
+tarot_cards = ["fool", "magic", "highpriestess", "empress", "emperor", "hierophant", "lovers", "chariot", "strength", "hermit", "wheel of fortune", "justice", "hangedman", "death",
                "temperance", "devil", "tower", "star", "moon", "sun", "judgment", "world"]
-
-
-
-
 
 def main():
     return None
@@ -64,11 +67,11 @@ def find_cards():
         for cardname, wordset in card_set_dict.items():
             if word in wordset:
                 card_score_dict[cardname] += 1
-    selected=sorted(card_score_dict, key=card_score_dict.get, reverse=True)[0:2]
+    selected=sorted(card_score_dict, key=card_score_dict.get, reverse=True)[0:3]
     return(selected)
 
 def give_answers(cards):
-    card_def={'fool':'leap of faith, step into the unknown', 'magician':'the tools are in front of you, you should act', 'high priestess':'hidden widsom covered by overthinking',
+    card_def={'fool':'leap of faith, step into the unknown', 'magician':'the tools are in front of you, you should act', 'highpriestess':'hidden widsom covered by overthinking',
               'empress':'nurture creativity and joy','emperor':'structure, boundaries, authority', 'hierophant':'there is value in tradition, but which rules are you following and why?',
               'lovers':'romance, integrity, follow your heart, at a crossroad', 'chariot':'move forward with purpose, steer with will and wisdom','strength':'tenderness can be strength. rise from struggle',
               'hermit':'retreat to reflect, inner wisdom','wheel of fortune':'change is the only constant. life has ups and downs','justice':'balance the scales, truth will prevail',
@@ -76,7 +79,9 @@ def give_answers(cards):
               'temperance':"don't rush outcomes, you can't control them", 'devil':'trapped in your own thoughts, a self-prison', 'tower':'sudden upheaval. current structure was built on shaky foundations.',
               'star':'hope after upheaval. healing, renewal.', 'moon':'truth will be revealed. cloudy at the moment, but trust your instincts.', 'sun':'optimism, release self-doubt. welcome joy into your life.',
               'judgment':'evolution, clarity, a rise to the next stage', 'world':'success, completion, reflection. the world is at your feet!'}
-
+    for cardpicked in cards:
+        print(cardpicked, ":")
+        print(card_def[cardpicked])
 if __name__=="__main__":
     cards_found=find_cards()
     give_answers(cards_found)
